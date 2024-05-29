e242db6787bf6464961f59bf403fce88bb75bc93
Charitha Weerasooriya <charithaw1234@gmail.com>
2024-05-29 19:53:01 +0530

    Modified ListGroup Component

--- src/Components/listGroup.tsx
+++ src/Components/listGroup.tsx
@@ -4,6 +4,7 @@ function ListGroup() {
   return (
     <>
       <ul className="list-group">
+        {items.length === 0 && <p>No items</p>}
         {items.map((item) => (
           <li className="list-group-item" key={item}>
             {item}

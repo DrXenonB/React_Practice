cd52992ba94940b3c2d48dfc6561a344ed50b771
Charitha Weerasooriya <charithaw1234@gmail.com>
2024-05-29 20:26:11 +0530

    Add Raect UseState function to ListGroup component

--- src/Components/listGroup.tsx
+++ src/Components/listGroup.tsx
@@ -1,17 +1,30 @@
-import { MouseEvent } from "react";
+import { useState } from "react";
 
 function ListGroup() {
   const items = ["First item", "Second item", "Third item", "Fourth item"];
 
+  //hook
+  const [selectedItem, setSelectedItem] = useState(-1);
+
   //event handler
-  const handleClick = (event: MouseEvent) => console.log(event);
+  const handleClick = (index: number) => {
+    setSelectedItem(index);
+  };
 
   return (
     <>
       <ul className="list-group">
         {items.length === 0 && <p>No items</p>}
-        {items.map((item) => (
-          <li className="list-group-item" key={item} onClick={handleClick}>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedItem === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => handleClick(index)}
+          >
             {item}
           </li>
         ))}

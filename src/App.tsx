59eaf0db69650cdb0a333de158f957ee16aef873
Manjitha <manjithawaa@gmail.com>
2024-06-05 17:09:46 +0530

    12 - Exercise

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,36 @@
 import { useState } from "react";
 import "./App.css";
-import Navbar from "./Components/navbar";
-import Cart from "./Components/cart";
 
 function App() {
-  const [cartItems, setCartItems] = useState(["Product 1", "Product 2"]);
+  const [cart, setcart] = useState({
+    discount: 0.1,
+    items: [
+      { id: 1, title: "Product 1", quantity: 1 },
+      { id: 2, title: "Product 2", quantity: 1 },
+    ],
+  });
 
   const handleClick = () => {
-    setCartItems([]);
+    //Sets the first items quantity to 2
+    setcart({
+      ...cart,
+      items: cart.items.map((item) =>
+        item.id === 1 ? { ...item, quantity: 2 } : item
+      ),
+    });
   };
 
   return (
     <div className="app">
-      <Navbar cartItemsCount={cartItems.length} />
-      <Cart cartItems={cartItems} onClear={handleClick} />
+      <h3>Cart</h3>
+      <ul>
+        {cart.items.map((item) => (
+          <li>
+            {item.title} /quantity: {item.quantity}
+          </li>
+        ))}
+      </ul>
+      <button onClick={handleClick}>Click</button>
     </div>
   );
 }

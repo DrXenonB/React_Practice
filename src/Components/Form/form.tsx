b809c3a8f0d2bc404307036e68ad90ea5fcf38a4
Manjitha <manjithawaa@gmail.com>
2024-06-09 03:14:04 +0530

    Applying validation

--- src/Components/Form/form.tsx
+++ src/Components/Form/form.tsx
@@ -1,7 +1,19 @@
 import { FieldValues, useForm } from "react-hook-form";
 
+interface FormData {
+  // For define the shape of the form
+  name: string;
+  age: number;
+}
+
 const Form = () => {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }, // Nested deStructure
+  } = useForm<FormData>();
+  /* "{ register, handleSubmit, formState: {errors} }" Here is an example for "Nested deStructure" in JS */
+  console.log(errors);
 
   const onSubmit = (data: FieldValues) => {
     console.log(data);
@@ -15,11 +27,18 @@ const Form = () => {
             Name
           </label>
           <input
-            {...register("name")}
+            {...register("name", { required: true, minLength: 3 })}
             id="name"
             type="text"
             className="form-control"
           />
+          {errors.name?.type === "required" && (
+            <p className="text-danger">The name field is required</p>
+            /* '?.' - "Optional chaining" in javaScript. The type propoerty is evaluated only if there is a property called 'name' */
+          )}
+          {errors.name?.type === "minLength" && (
+            <p className="text-danger">The name is too short</p>
+          )}
         </div>
 
         <div className="mb-3">
@@ -27,11 +46,14 @@ const Form = () => {
             Age
           </label>
           <input
-            {...register("age")}
+            {...register("age", { required: true })}
             id="age"
             type="number"
             className="form-control"
           />
+          {errors.age?.type === "required" && (
+            <p className="text-danger">The age field is required</p>
+          )}
         </div>
 
         <button type="submit" className="btn btn-secondary">

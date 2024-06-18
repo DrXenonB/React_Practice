9c1edbd86ef9963fe068867c3a3c2c3861e60186
Manjitha <manjithawaa@gmail.com>
2024-06-19 00:30:01 +0530

    Creating a Custom Data fetching hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,8 @@
-import { CanceledError } from "./Services/api-client";
-import { useEffect, useState } from "react";
 import userService, { User } from "./Services/user-service";
+import useUsers from "./hooks/useUsers";
 
 function App() {
-  const [users, setUsers] = useState<User[]>([]);
-  const [error, setError] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    setIsLoading(true);
-    const { request, cancel } = userService.getAll<User>();
-    request
-      .then((res) => {
-        setUsers(res.data);
-        setIsLoading(false);
-      })
-      .catch((err) => {
-        if (err instanceof CanceledError) return;
-        setError(err.message);
-        setIsLoading(false);
-      });
-
-    return () => cancel();
-  }, []);
+  const { users, error, isLoading, setUsers, setError } = useUsers();
 
   const deleteUser = (user: User) => {
     const originalUsers = [...users];
---
+++ src/hooks/useUsers.ts
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import userService, { User } from "../Services/user-service";
+import { CanceledError } from "../Services/api-client";
+
+const useUsers = () => {
+  const [users, setUsers] = useState<User[]>([]);
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    const { request, cancel } = userService.getAll<User>();
+    request
+      .then((res) => {
+        setUsers(res.data);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+        setIsLoading(false);
+      });
+
+    return () => cancel();
+  }, []);
+
+  return { users, error, isLoading, setUsers, setError };
+};
+
+export default useUsers;
